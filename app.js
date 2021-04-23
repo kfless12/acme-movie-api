862f44bb271fbf145c3e0b8a4e8ae8a944fb56cc
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-04-22 20:39:03 -0400

    added movies test

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
+const { models } = require('./db');
 
-module.exports = app
+module.exports = app;
 
-app.get('/', (req, res, next)=>{
-    res.send(`
+app.get('/', (req, res, next) => {
+  res.send(`
     <html>
         <body>
             The Acme API
         </body>
     </html>
-        `)
-})
+        `);
+});
+
+app.get('/api/movies', async (req, res, next) => {
+  try {
+    res.send(await models.Movie.findAll());
+  } catch (error) {
+    next(error);
+  }
+});
--- test/spec.js
+++ test/spec.js
@@ -1,13 +1,23 @@
 const { expect } = require('chai');
+const { syncAndSeed } = require('../db');
 
 const app = require('supertest')(require('../app'));
 
-describe('Routes', function() {
-  describe('first route check  (/)', function() {
-    it('should show the text sent from / route', async()=>{
+describe('Routes', function () {
+  beforeEach(() => syncAndSeed());
+  describe('first route check  (/)', function () {
+    it('should show the text sent from / route', async () => {
       const response = await app.get('/');
       expect(response.status).to.equal(200);
       expect(response.text).to.include('The Acme API');
+    });
+  });
+
+  describe('first route check  (/)', function () {
+    it('should show all Movies', async () => {
+      const response = await app.get('/api/movies');
+      expect(response.status).to.equal(200);
+      expect(response.body.length).to.equal(4);
     });
   });
 });
